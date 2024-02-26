e635dc986e1561061895c7f2fb52b39f19fc2d18
ggl <ggl@users.noreply.github.com>
2024-02-26 11:31:21 +0200

    Add pt and en locales, change formats

---
+++ src/js/locales/en.ts
@@ -0,0 +1,44 @@
+const name = 'en';
+
+const localization = {
+  today: 'Today',
+  clear: 'Clear selection',
+  close: 'Close selector',
+  selectMonth: 'Select month',
+  previousMonth: 'Previous month',
+  nextMonth: 'Next month',
+  selectYear: 'Select year',
+  previousYear: 'Previous year',
+  nextYear: 'Next year',
+  selectDecade: 'Select decade',
+  previousDecade: 'Previous decade',
+  nextDecade: 'Next decade',
+  previousCentury: 'Previous century',
+  nextCentury: 'Next century',
+  pickHour: 'Pick hour',
+  incrementHour: 'Increment hour',
+  decrementHour: 'Decrement hour',
+  pickMinute: 'Pick minute',
+  incrementMinute: 'Increment minute',
+  decrementMinute: 'Decrement minute',
+  pickSecond: 'Pick second',
+  incrementSecond: 'Increment second',
+  decrementSecond: 'Decrement second',
+  toggleMeridiem: 'Toggle AM/PM',
+  selectTime: 'Select time',
+  selectDate: 'Select date',
+  dayViewHeaderFormat: { month: 'long', year: '2-digit' },
+  startOfTheWeek: 1,
+  locale: 'en',
+  hourCycle: 'h24',
+     dateFormats: {
+     LTS: 'hh:mm:ss',
+     LT: 'hh:mm',
+     L: 'yyyy-MM-dd',
+     LLL: 'yyyy-MM-dd hh:mm:ss'
+  },
+  ordinal: (n) => `${n}`,
+  format: 'L LT',
+};
+
+export { localization, name };
---
+++ src/js/locales/pt.ts
@@ -0,0 +1,44 @@
+const name = 'pt';
+
+const localization = {
+  today: 'Hoje',
+  clear: 'Limpar seleção',
+  close: 'Fechar seletor',
+  selectMonth: 'Selecionar mês',
+  previousMonth: 'Mês anterior',
+  nextMonth: 'Próximo mês',
+  selectYear: 'Selecionar ano',
+  previousYear: 'Ano anterior',
+  nextYear: 'Próximo ano',
+  selectDecade: 'Selecionar década',
+  previousDecade: 'Década anterior',
+  nextDecade: 'Próxima década',
+  previousCentury: 'Século anterior',
+  nextCentury: 'Próximo século',
+  pickHour: 'Escolher hora',
+  incrementHour: 'Incrementar hora',
+  decrementHour: 'Decrementar hora',
+  pickMinute: 'Escolher minuto',
+  incrementMinute: 'Incrementar minuto',
+  decrementMinute: 'Decrementar minuto',
+  pickSecond: 'Escolher segundo',
+  incrementSecond: 'Incrementar segundo',
+  decrementSecond: 'Decrementar segundo',
+  toggleMeridiem: 'Alternar AM/PM',
+  selectTime: 'Selecionar hora',
+  selectDate: 'Selecionar data',
+  dayViewHeaderFormat: { month: 'long', year: '2-digit' },
+  startOfTheWeek: 1,
+  locale: 'pt',
+  hourCycle: 'h24',
+  dateFormats: {
+	LTS: 'hh:mm:ss',
+	LT: 'hh:mm',
+	L: 'yyyy-MM-dd',
+	LLL: 'yyyy-MM-dd hh:mm:ss'
+  },
+  ordinal: (n) => `${n}º`,
+  format: 'L LLL',
+};
+
+export { localization, name };
